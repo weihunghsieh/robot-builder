8a7c378a64a23b76c2e642e70ef0398836a3c384
Colby Tresness <cotresne@microsoft.com>
2019-06-24 21:30:18 -0700

    fixes

--- src/parts/PartInfo.vue
+++ src/parts/PartInfo.vue
@@ -6,16 +6,26 @@
 </template>
 
 <script>
-import parts from "../data/parts";
 
 export default {
   name: "PartInfo",
+  parts: {},
   computed: {
     part() {
       const { partType, id } = this.$route.params;
 
       return parts[partType].find(part => part.id === +id);
     }
+  },
+  mounted () {
+    axios
+    .get('https://angry-babbage-98ec47.netlify.com/.netlify/functions/partlist')
+    .then(response => {
+      this.availableParts = response.data;
+      for (var head in this.availableParts.heads){
+        console.log(head);   
+      }
+    });
   }
 };
 </script>
