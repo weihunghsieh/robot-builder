de8b685ba84a8aee163b1b378b1ff7abbfac8520
Colby Tresness <cotresne@microsoft.com>
2019-06-24 21:20:14 -0700

    fixing build error

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -102,9 +102,6 @@ export default {
     .get('https://angry-babbage-98ec47.netlify.com/.netlify/functions/partlist')
     .then(response => {
       this.availableParts = response.data;
-      for (var head in this.availableParts.heads) {
-        head.images
-      }
     });
   },
   mixins: [createdHookMixin],
